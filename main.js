a409068edcdd2f87157f294166963629c263957d
sungjae77 <wisdomguy77@gmail.com>
2021-01-06 15:35:08 +0900

    a-a

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function git(){
 	console.log('0'); //remote
 	console.log('a'); //local
+	console.log('a-a'); //local
 	console.log('b'); //local
 	console.log('c'); //remote
 	console.log('d'); //local
