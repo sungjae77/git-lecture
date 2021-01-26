627ebd91f94dbda5d28503a03a09f2e024056fcc
조성재 <@sk.com>
2021-01-26 14:41:29 +0900

    init

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
 function git(){
-    console.log('0'); //1
-	console.log('1'); //2
-	console.log('2'); //3
-	console.log('3'); //4
-	console.log('3'); //4
-	console.log('3'); //4
-	console.log('4'); //7
-	console.log('8'); //8
-	console.log('local 에서 겁나 중요한 업무');
-	console.log('원격의 소스가 바뀐 상태인데 푸시가 될 푸시될까?');
-	//remote modify
-	//local modify
-	//remote modify 2
+    console.log('0'); //0
 }
