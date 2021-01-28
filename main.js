07df3154ceaf3d156d7e688ed09c8f6c7cbec42a
조성재-vscode <wisdomguy-vscode@sk.com>
2021-01-28 14:43:54 +0900

    restart

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
 function git(){
     console.log('0'); //0
-    console.log('사이에 넣다'); //remote by 이정민
-    console.log('5'); //local by 조성재
-    console.log('1'); //remote by 이정민
-    console.log('2'); //local by 조성재
-	console.log('5'); //sts
-    console.log('6'); //intellij
-    console.log('6-1'); // local
-    console.log('7'); // 이정민
-    console.log('8');
-	conosle.log('8-1');
-	console.log('9');
-	console.log('10');
-	console.log('급해');
 }
